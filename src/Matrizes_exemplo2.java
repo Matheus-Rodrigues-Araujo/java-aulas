006c6ce9c6eb217a1b734888f52b94dd288ce8cc
Matheus Rodrigues Araujo <68081476+Matheus-Rodrigues-Araujo@users.noreply.github.com>
2024-07-23 15:05:45 -0300

    first commit

---
+++ src/Matrizes_exemplo2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Matrizes_exemplo2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+
+        System.out.print("Número linhas x colunas: ");
+        int lines = sc.nextInt();
+        int columns = sc.nextInt();
+
+        int[][] matriz = new int[lines][columns];
+
+        for (int i = 0; i < lines; i++) {
+            for (int j = 0; j < columns; j++) {
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+        System.out.print("Number of choice: ");
+        int number = sc.nextInt();
+        System.out.println();
+
+        for (int i = 0; i < lines; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matriz[i][j] == number) {
+                    System.out.println("Position: " + i + ", " + j);
+                    if (j - 1 > 0) {
+                        System.out.println("Left: " + matriz[i][j -1]);
+                    }
+                    if (j + 1 < columns){
+                        System.out.println("Right: " + matriz[i][j + 1]);
+                    }
+                    if (i + 1 < lines){
+                        System.out.println("Down: " + matriz[i + 1][j]);
+                    }
+                    if (i - 1 > 0){
+                        System.out.println("Up: " + matriz[i + 1][j]);
+                    }
+
+                }
+            }
+        }
+
+    }
+}
---
+++ src/Matrizes_exemplo_1.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class Matrizes_exemplo_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+
+        System.out.print("Número linhas x colunas: ");
+        int n = sc.nextInt();
+
+        int[][] matriz = new int[n][n];
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("-------------------------");
+
+        System.out.println("MATRIZ");
+        for(int i = 0; i < matriz.length; i++){
+            for(int j = 0; j < matriz[i].length; j++){
+                System.out.print(matriz[i][j] + " " );
+            }
+            System.out.println();
+        }
+        System.out.println("-------------------------");
+        System.out.println("MAIN DIAGONAL:");
+        for (int i = 0; i < matriz.length; i++) {
+            System.out.print(matriz[i][i] + " ");
+        }
+        System.out.println();
+        sc.close();
+
+        System.out.println("-------------------------");
+
+        System.out.println("NEGATIVE NUMBERS:");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] < 0 ) {
+                    System.out.print(matriz[i][j] + " ");
+                    count++;
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("Total = " + count);
+
+    }
+}
